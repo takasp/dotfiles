c00c765fa99684f927ae7346259a854be6d661dd
takasp <takasp.2350@gmail.com>
2021-03-03 21:38:57 +0900

    Fix to use $HOME

--- etc/init/dotfiles.sh
+++ etc/init/dotfiles.sh
@@ -2,7 +2,7 @@
 
 set -euC
 
-ROOT_DIR="~/dev/src/github.com/takasp"
+ROOT_DIR="$HOME/dev/src/github.com/takasp"
 mkdir -p $ROOT_DIR
 git clone --recursive https://github.com/takasp/dotfiles.git $ROOT_DIR/dotfiles
 cd $ROOT_DIR/dotfiles
